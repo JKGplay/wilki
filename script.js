f339ac1dab6498cb2207e9ed787b8b63a5d36509
gazda <jkgplay2005@gmail.com>
2022-01-30 22:52:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,15 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 const myForm = document.getElementById("form");
 const btn = document.getElementById("btn");
 const span = document.getElementById("result");
+const knocked = document.getElementById("knocked");
+let knockedBool = false
 
 submit();
 
 function submit() {
-    if (!isNaN(myForm.velNum.value) && !isNaN(myForm.AC.value) && myForm.velNum.value > 0 && myForm.AC.value > 0) {
-        span.innerHTML = numRaptor();
+    if (!isNaN(myForm.velNum.value) && !isNaN(myForm.AC.value) && !isNaN(myForm.RO.value) && myForm.velNum.value > 0 && myForm.AC.value > 0) {
+        knockedBool = false;
+	span.innerHTML = numWolf();
     } else {
         span.innerHTML = "Błąd";
     }
@@ -31,63 +34,60 @@ function rand(min, max) {
 }
 
 
-function raptor1() {
-    let k20_1 = rand(1, 20);
-    let k20_2 = rand(1, 20);
-    let dmg1 = 0;
-    let dmg2 = 0;
-    let dmg3 = 0;
+function wolf1() {
+    let k20 = rand(1, 20);
+    let RO = rand(1, 20) + parseInt(myForm.RO.value);
+    let dmg = 0;
+    knocked.innerHTML = "Nie";
 
-    if (k20_1 == 20) {
-        dmg2 += rand(1, 6) + rand(1, 6) + 2;
-    } else if (k20_1 + 4 >= myForm.AC.value) {
-        dmg2 += rand(1, 6) + 2;
+    if (k20 == 20) {
+        dmg += rand(1, 4) + rand(1, 4) + rand(1, 4) + rand(1, 4) + 2;
+	if (RO >= 11) {
+	    knocked.innerHTML = "Tak";
+	}
+    } else if (k20 + 4 >= myForm.RO.value) {
+        dmg += rand(1, 4) + rand(1, 4) + 2;
+	if (RO >= 11) {
+	    knocked.innerHTML = "Tak";
+	}
     }
-
-    if (k20_2 == 20) {
-        dmg3 += rand(1, 4) + rand(1, 4) + 2;
-    } else if (k20_2 + 4 >= myForm.AC.value) {
-        dmg3 += rand(1, 4) + 2;
-    }
-
-    dmg1 = dmg2 + dmg3;
-    return dmg1;
+    return dmg;
 }
 
-function raptor2() {
+function wolf2() {
     let k20_1 = rand(1, 20);
     let k20_2 = rand(1, 20);
-    let k20_3 = rand(1, 20);
-    let k20_4 = rand(1, 20);
-    let dmg1 = 0;
-    let dmg2 = 0;
-    let dmg3 = 0;
+    let RO = rand(1, 20) + parseInt(myForm.RO.value);
+    let dmg = 0;
+    if (knockedBool === false) {
+	knocked.innerHTML = "Nie";
+    }
 
     if ((k20_1 === 20) || (k20_2 === 20)) {
-        dmg2 += rand(1, 6) + rand(1, 6) + 2;
+        dmg += rand(1, 4) + rand(1, 4) + rand(1, 4) + rand(1, 4) + 2;
+	if (RO >= 11 && knockedBool === false) {
+	    knocked.innerHTML = "Tak";
+	    knockedBool = true;
+	}
     } else if ((k20_1 + 4 >= myForm.AC.value) || (k20_2 + 4 >= myForm.AC.value)) {
-        dmg2 += rand(1, 6) + 2;
+        dmg += rand(1, 4) + rand(1, 4) + 2;
+	if (RO >= 11 && knockedBool === false) {
+	    knocked.innerHTML = "Tak";
+	    knockedBool = true;
+	}
     }
-
-    if ((k20_3 === 20) || (k20_4 === 20)) {
-        dmg3 += rand(1, 4) + rand(1, 4) + 2;
-    } else if ((k20_3 + 4 >= myForm.AC.value) || (k20_4 + 4 >= myForm.AC.value)) {
-        dmg3 += rand(1, 4) + 2;
-    }
-
-    dmg1 = dmg2 + dmg3;
-    return dmg1;
+    return dmg;
 }
 
-function numRaptor() {
+function numWolf() {
     let dmgAll = 0;
 
     if (myForm.velNum.value > 1) {
         for (let i = 0; i < myForm.velNum.value; i++) {
-            dmgAll += raptor2();
+            dmgAll += wolf2();
         }
     } else {
-        dmgAll = raptor1();
+        dmgAll = wolf1();
     }
 
     return dmgAll;
@@ -112,4 +112,14 @@ rangeAC.addEventListener('input', function (e) {
 });
 fieldAC.addEventListener('input', function (e) {
     rangeAC.value = e.target.value;
+});
+
+var rangeRO = document.querySelector('.inputRangeRO');
+var fieldRO = document.getElementById('RO');
+
+rangeRO.addEventListener('input', function (e) {
+    fieldRO.value = e.target.value;
+});
+fieldRO.addEventListener('input', function (e) {
+    rangeRO.value = e.target.value;
 });
